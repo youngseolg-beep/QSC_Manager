a71c40dc3d871a57ac23c8f33367216857c15c04
youngseolg-beep <youngseolg@gmail.com>
2026-08-04 12:16:55 +0900

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fdbxikqwzingtzcptfsh.supabase.co/rest/v1/';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkYnhpa3F3emluZ3R6Y3B0ZnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4MDk1MzgsImV4cCI6MjEwMTM4NTUzOH0.9BwBhuVVI84IwIE_mKooQ22VgVtu6Js-e4p9BAM08LU';
+// Supabase 대시보드 URL & Anon Key
+const supabaseUrl = 'https://vyilbllhllxndlyzskfh.supabase.co'.trim();
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5aWxibGxobGx4bmRseXpza2ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzIzODMsImV4cCI6MjA1NTc0ODM4M30.S66v9wGkUIsi8K8j-IAtE0B-fAypQZpt0rT8lVdIOfk'.trim();
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
