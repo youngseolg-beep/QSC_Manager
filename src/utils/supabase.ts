9c5ce2e717422e36b81e4270728fd2ab3e3fccc5
youngseolg-beep <youngseolg@gmail.com>
2026-08-04 12:47:39 +0900

    Update supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase Dashboard -> Project Settings -> API에서 확인 가능한 정확한 값
-const supabaseUrl = 'https://vyilbllhllxndlyzskfh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5aWxibGxobGx4bmRseXpza2ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzIzODMsImV4cCI6MjA1NTc0ODM4M30.S66v9wGkUIsi8K8j-IAtE0B-fAypQZpt0rT8lVdIOfk';
+const supabaseUrl = 'https://fdbxikqwzingtzcptfsh.supabase.co/rest/v1/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkYnhpa3F3emluZ3R6Y3B0ZnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4MDk1MzgsImV4cCI6MjEwMTM4NTUzOH0.9BwBhuVVI84IwIE_mKooQ22VgVtu6Js-e4p9BAM08LU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
